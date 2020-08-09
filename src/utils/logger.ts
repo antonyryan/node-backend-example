14a0a0e7261bf92627b996c022f365a43163beb8
Ryan <antonyryanr@gmail.com>
2020-08-09 20:55:02 -0300

    ✨ Add logger

---
+++ src/utils/logger.ts
@@ -0,0 +1,30 @@
+import winston from 'winston';
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json(),
+  ),
+  transports: [
+    new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'logs/info.log' }),
+  ],
+});
+
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(new winston.transports.Console({
+    level: 'silly',
+    format: winston.format.combine(
+      winston.format.timestamp(),
+      winston.format.colorize(),
+      winston.format.simple(),
+    ),
+  }));
+}
+
+if (process.env.NODE_ENV === 'test') {
+  logger.silent = true;
+}
+
+export default logger;
