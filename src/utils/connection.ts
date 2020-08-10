b1d91b883005bb0e496817a17dc4bcd955e4c64d
Ryan <antonyryanr@gmail.com>
2020-08-10 17:50:03 -0300

    :sparkles: Add migrations settings

--- src/utils/connection.ts
+++ src/utils/connection.ts
@@ -1,4 +1,5 @@
 import { createConnection, Connection } from 'typeorm';
+import { Client } from '../components/clients';
 
 export const connection: Promise<Connection> = createConnection({
   type: 'mysql',
@@ -7,4 +8,7 @@ export const connection: Promise<Connection> = createConnection({
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
+  entities: [Client],
+  migrations: ['src/migration/*.ts'],
+  cli: { migrationsDir: 'migration' },
 });
