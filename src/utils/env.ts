b63fdabdd19187605dd1b50d97f9b966824e16af
Ryan <antonyryanr@gmail.com>
2020-08-12 16:03:14 -0300

    :bug: Fixing env variables to work on test env

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,5 +1,5 @@
 import { config } from 'dotenv';
 
-const path = process.env.NODE_ENV === 'development' ? '.env.dev' : '.env';
+const path = process.env.NODE_ENV === 'production' ? '.env' : '.env.dev';
 
 config({ path });
